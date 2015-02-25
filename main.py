08c17c7a007980a863e99dfe86f49cffbef82fd0
siddeekazeez <siddeekazeez@gmail.com>
2015-02-25 16:44:28 +0000

    python script corrected

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 from google.appengine.api import users
 import webapp2
 import jinja2
@@ -100,9 +84,9 @@ class TimeSwitch(webapp2.RequestHandler):
                 'time': timet,
                 'date': datet,
                 'timelink': '/',
-                'datelink': '/DateSwitch',
-
+                'datelink': '/DateTimeSwitch',
             }
+            
             self.response.write(template.render(template_values))
         else:
             self.redirect(users.create_login_url(self.request.uri))
@@ -134,7 +118,7 @@ class TimeSwitch(webapp2.RequestHandler):
                 'time': timet,
                 'date': datet,
                 'timelink': '/',
-                'datelink': '/DateSwitch',
+                'datelink': '/DateTimeSwitch',
                 'emailmsg': email_message,
             }
 
@@ -157,7 +141,7 @@ class DateSwitch(webapp2.RequestHandler):
                 'url_logout_text': 'Log out',
                 'time': timet,
                 'date': datet,
-                'timelink': '/',
+                'timelink': '/DateTimeSwitch',
                 'datelink': '/',
 
             }
@@ -191,7 +175,7 @@ class DateSwitch(webapp2.RequestHandler):
                 'url_logout_text': 'Log Out',
                 'time': timet,
                 'date': datet,
-                'timelink': '/',
+                'timelink': '/DateTimeSwitch',
                 'datelink': '/',
                 'emailmsg': email_message,
             }
@@ -215,8 +199,8 @@ class DateTimeSwitch(webapp2.RequestHandler):
                 'url_logout_text': 'Log out',
                 'time': timet,
                 'date': datet,
-                'timelink': '/',
-                'datelink': '/',
+                'timelink': '/DateSwitch',
+                'datelink': '/TimeSwitch',
 
             }
             self.response.write(template.render(template_values))
@@ -249,8 +233,8 @@ class DateTimeSwitch(webapp2.RequestHandler):
                 'url_logout_text': 'Log Out',
                 'time': timet,
                 'date': datet,
-                'timelink': '/',
-                'datelink': '/',
+                'timelink': '/DateSwitch',
+                'datelink': '/TimeSwitch',
                 'emailmsg': email_message,
             }
 
@@ -263,5 +247,5 @@ app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/TimeSwitch', TimeSwitch),
     ('/DateSwitch', DateSwitch),
-    ('/DateTimeSwitch', DateTimeSwitch)
+    ('/DateTimeSwitch', DateTimeSwitch),
 ], debug=True)
